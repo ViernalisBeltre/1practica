a20abb46e0d4884c0fd5c231d010038bccc84121
ViernalisBeltre <beltreperezviernalis@gmail.com>
2025-09-26 05:13:30 +0200

    Ejercicio1

---
+++ ACTIVIDADunida1/ACTIVIDADunida1/Controllers/PresentacionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ACTIVIDADunida1.Controllers
+{
+    public class 
+        PresentacionController : Controller
+    {
+        public IActionResult Saludo()
+
+        {
+           
+            ViewBag.Mensaje= "Hola!! me llamo Viernalis,Mi Matricula es SD-2022-0400";
+
+            return View();
+        }
+    }
+}
+  
